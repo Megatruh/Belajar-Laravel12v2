f70d6c90c533f071f4baae815496b7d5d4bdf2b8
Shatramaja Parrot <farhaneshaputra132@gmail.com>
2026-04-09 23:31:22 +0700

    aman sahaja

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,9 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -14,11 +15,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Farhan Esha Putra',
-            'username' => strtolower(str_replace(' ', '', 'Farhan Esha Putra')) . 132,
-            'email' => strtolower('FarhanEshaPutra132') . '@gmail.com',
-            'password' => Hash::make('plmoki09'),
+        User::create([
+            'name'=> env('ADMIN_NAME'),
+            'username'=>env('ADMIN_USERNAME'),
+            'email'=>env('ADMIN_EMAIL'),
+            'email_verified_at' => now(),
+            'password'=>Hash::make(env('ADMIN_PASSWORD')),
+            'remember_token' => Str::random(10),
+
         ]);
 
         User::factory(7)->create();
